426cdff6c14a3983371968a065713eb27332648e
ajaymehul <ajaymehul2013@gmail.com>
2020-02-16 15:49:45 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# gazebo-lsl
+
+This is a Gazebo plugin that allows models to stream data to Lab Streaming Layer.
+Tested on Ubuntu  18.04.3
+
+
+## Prerequisites
+
+ - Gazebo 9.0.0 [Installation Intructions](http://gazebosim.org/tutorials?tut=install_ubuntu&cat=install)
+ - liblsl 13.0.0 [Releases Link](https://github.com/sccn/liblsl/releases) [Tested: Release b-12]
+ - Ubuntu 18.04.x LTS [Download](https://ubuntu.com/download/desktop)
+ 
+## Build Instructions
+
+Make sure Gazebo and liblsl are properly installed.  Use CMake to build the plugin. 
+
+To find the appropriate Gazebo libraries and link them, use the following:
+   ```cpp
+cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
+
+find_package(gazebo REQUIRED)
+include_directories(${GAZEBO_INCLUDE_DIRS})
+link_directories(${GAZEBO_LIBRARY_DIRS})
+list(APPEND CMAKE_CXX_FLAGS "${GAZEBO_CXX_FLAGS}")
+```
+
+To find and link liblsl, use the following:
+   ```cpp
+find_package(LSL REQUIRED
+      HINTS ${LSL_INSTALL_ROOT}
+      "${CMAKE_CURRENT_LIST_DIR}/../../LSL/liblsl/build/"
+      "${CMAKE_CURRENT_LIST_DIR}/../../LSL/liblsl/build/install"
+      PATH_SUFFIXES share/LSL
+)
+```
+Then you can link your target with Gazebo and liblsl with:
+
+   ```cpp
+target_link_libraries(${PROJECT_NAME} PUBLIC ${GAZEBO_LIBRARIES} LSL::lsl)
+```
+    
+
+
+## Usage
+
+he built plugin to any model in your gazebo world by adding the plugin tag in your world file. The plugin uses the Gazebo model ID as the source_id for LSL, so you can add as many instances of the plugin to different models in the world.
+
+The built plugin from the example can be added using:
+
+    <plugin name="lsl_publish" filename="liblsl_publish.so"/>
+
+The example publishes the XYZ coordinates of the model. You can customize the data published by making changes to the `OnUpdate()` method in the plugin.  
+
+## Running the example
+
+ - Make sure Gazebo and liblsl are installed properly.  
+ - Clone this repository and go into the directory.c
+ `$ git clone https://github.com/ajaymehul/gazebo_lsl.git ~/gazebo_lsl`
+ - Create the `build` directory:
+`$ mkdir ~/gazebo_lsl/build`
+`$ cd ~/gazebo_lsl/build`
+
+ - Compile the code
+`$ cmake ../`
+`$ make`
+- This will compile the plugin to ``~/gazebo_lsl/build/libhello_world.so``
+- Export the plugin path so that Gazebo can use it
+`$ export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:~/gazebo_plugin_tutorial/build`
+- Launch the example world using
+` gzserver ~/gazebo_lsl/example.world --verbose`
+- Use [Lab Recorder](https://github.com/labstreaminglayer/App-LabRecorder) or any another app to get the data.
+
+
---
+++ lsl_publish.cc
@@ -0,0 +1,92 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+#include <ignition/math/Pose3.hh>
+#include "lsl_cpp.h"
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+
+
+namespace gazebo
+{
+  class LSLPublish : public ModelPlugin
+  {
+    public: LSLPublish() : ModelPlugin()
+            {
+              printf("Hello World!\n");
+            }
+
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
+    { printf("Hello World!\n");
+      // Store the pointer to the model
+      this->model = _parent;
+
+
+      //Setup LSL outlet connection
+      try {
+        //Make a new stream_info (100Hz)
+        //info(name, type, channel_count, sample_rate, format, source_id)
+        lsl::stream_info info("Gazebo","Model",3,lsl::IRREGULAR_RATE,lsl::cf_float32, std::to_string(this->model->GetId()));
+        //Add description fields
+        //Add units fields
+        info.desc().append_child_value("Units", "Meters");
+        //Add Model Name
+        info.desc().append_child_value("Model Name", this->model->GetName());
+        //Add Model ID
+        info.desc().append_child_value("Model ID", std::to_string(this->model->GetId()));
+
+
+        //Create outlet  
+        this->outlet = new lsl::stream_outlet(info);
+        
+      } 
+      catch(std::exception &e) {
+		    std::cerr << "Got an exception: " << e.what() << std::endl;
+	    }
+
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&LSLPublish::OnUpdate, this));
+         
+    }
+
+    // Called by the world update start event
+    public: void OnUpdate()
+    {
+      // Push Pose of this->model to LSL outlet
+       // Get pose of this->model
+      ignition::math::Pose3d pose = this->model->WorldPose();
+      // Get position vector (XYZ coordinates)
+      ignition::math::Vector3<double> v= pose.Pos();
+
+      //Create sample packet and populate with XYZ coordinates
+      float sample[3];
+      sample[0] = v.X();
+      sample[1] = v.Y();
+      sample[2] = v.Z();
+
+      //send the sample
+      try {
+        this->outlet->push_sample(sample);
+      }
+      catch(std::exception &e) {
+		    std::cerr << "Got an exception: " << e.what() << std::endl;
+	    }
+    }
+
+    // Pointer to the model
+    private: physics::ModelPtr model;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
+
+    public: lsl::stream_outlet* outlet;
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(LSLPublish)
+}
